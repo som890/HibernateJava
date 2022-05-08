b4ea813001e1ca9de27f3d4382d921a60cadcbb1
Lo Van Som <lovansomst@gmail.com>
2022-05-08 20:36:16 +0700

    first commit

---
+++ Hibernate_Enities/src/main/java/lovansom/Hibernate_Enities/App.java
@@ -0,0 +1,13 @@
+package lovansom.Hibernate_Enities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
